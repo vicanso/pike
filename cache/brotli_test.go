eb1e5204d9fa76630c1e6312f15ba13a8095c675
vicanso <vicansocanbico@gmail.com>
2019-05-26 20:56:51 +0800

    test: fix brotli compress test

--- cache/brotli_test.go
+++ cache/brotli_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestBrotliCompress(t *testing.T) {
 	assert := assert.New(t)
-	buf, err := doBrotli([]byte("abcd"))
+	buf, err := doBrotli([]byte("abcd"), 0)
 	assert.Nil(err, "brotli compress fail")
 	assert.NotEqual(len(buf), 0, "brotli compress fail")
 }
