17329dfee185944a55f01c932783b3c349f3f3f7
vicanso <vicansocanbico@gmail.com>
2019-10-25 21:51:48 +0800

    feat: add `cachable` for http cache

--- cache/http_cache.go
+++ cache/http_cache.go
@@ -18,7 +18,7 @@
 package cache
 
 import (
-	"fmt"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -36,28 +36,86 @@ const (
 	StatusPassed
 )
 
-// HTTPCache cache status
-type HTTPCache struct {
-	mu     sync.Mutex
-	status int
-	chans  []chan bool
+type (
+	// HTTPHeader http header
+	HTTPHeader [][]byte
+	// HTTPHeaders http headers
+	HTTPHeaders []HTTPHeader
+	// HTTPData http data
+	HTTPData struct {
+		// Header
+		Headers    HTTPHeaders
+		StatusCode int
+		GzipBody   []byte
+		BrBody     []byte
+		RawBody    []byte
+	}
+	// HTTPCache cache status
+	HTTPCache struct {
+		mu        sync.Mutex
+		status    int
+		chans     []chan bool
+		data      *HTTPData
+		createdAt int
+		expiredAt int
+	}
+)
+
+// NewHTTPHeader new a http header
+func NewHTTPHeader(key, value []byte) HTTPHeader {
+	header := make([][]byte, 2)
+	header[0] = key
+	header[1] = value
+	return header
 }
 
-// NewHTTPCache new http cache
+// NewHTTPHeaders new a http headers
+func NewHTTPHeaders(header http.Header, ignoreHeaders ...string) (headers HTTPHeaders) {
+	headers = make(HTTPHeaders, 0, 10)
+	for key, values := range header {
+		ignore := false
+		for _, ignoreHeader := range ignoreHeaders {
+			if ignoreHeader == key {
+				ignore = true
+			}
+		}
+		if ignore {
+			continue
+		}
+		k := []byte(key)
+		for _, value := range values {
+			v := []byte(value)
+			h := NewHTTPHeader(k, v)
+			headers = append(headers, h)
+		}
+	}
+	return
+}
+
+// NewHTTPCache new a http cache
 func NewHTTPCache() *HTTPCache {
 	return &HTTPCache{}
 }
 
-// GetStatus get http status
-func (hc *HTTPCache) GetStatus() (status int, done chan bool) {
+// Get get http cache
+func (hc *HTTPCache) Get() (status int, data *HTTPData) {
+	status, done, data := hc.get()
+	if done != nil {
+		<-done
+		status = hc.status
+		data = hc.data
+	}
+	return
+}
+
+// get get http cache
+func (hc *HTTPCache) get() (status int, done chan bool, data *HTTPData) {
 	hc.mu.Lock()
 	defer hc.mu.Unlock()
-	fmt.Println(time.Now().Unix())
-	// TODO 如果缓存已过期，设置为StatusUnknown
-
-	if hc.status == StatusUnknown {
-		hc.status = StatusFetching
-		hc.chans = make([]chan bool, 0, 5)
+	now := int(time.Now().Unix())
+	// 如果缓存已过期，设置为StatusUnknown
+	if hc.expiredAt != 0 && hc.expiredAt < now {
+		hc.status = StatusUnknown
 	}
 	// 如果是fetching，则相同的请求需要等待完成
 	// 通过chan bool返回完成
@@ -65,6 +123,47 @@ func (hc *HTTPCache) GetStatus() (status int, done chan bool) {
 		done = make(chan bool)
 		hc.chans = append(hc.chans, done)
 	}
+
+	if hc.status == StatusUnknown {
+		hc.status = StatusFetching
+		hc.chans = make([]chan bool, 0, 5)
+	}
+
 	status = hc.status
+	// 为什么需要返回status与data
+	// 因为有可能在函数调用完成后，刚好缓存过期了，如果此时不返回status与data
+	// 当其它goroutin获取锁之后，有可能刚好重置数据
+	if status == StatusCachable {
+		data = hc.data
+	}
 	return
+}
+
+// HitForPass set the http cache hit for pass
+func (hc *HTTPCache) HitForPass(ttl int) {
+	hc.mu.Lock()
+	defer hc.mu.Unlock()
+	hc.expiredAt = int(time.Now().Unix()) + ttl
+	hc.status = StatusHitForPass
+	for _, ch := range hc.chans {
+		ch <- true
+	}
+}
+
+// Cachable set the http cache cachable
+func (hc *HTTPCache) Cachable(ttl, statusCode int, rawBody, gzipBody, brBody []byte) {
+	hc.mu.Lock()
+	defer hc.mu.Unlock()
+	hc.createdAt = int(time.Now().Unix())
+	hc.expiredAt = hc.createdAt + ttl
+	hc.status = StatusCachable
+	hc.data = &HTTPData{
+		StatusCode: statusCode,
+		GzipBody:   gzipBody,
+		BrBody:     brBody,
+		RawBody:    rawBody,
+	}
+	for _, ch := range hc.chans {
+		ch <- true
+	}
 }
--- cache/http_cache_test.go
+++ cache/http_cache_test.go
@@ -11,3 +11,100 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+package cache
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTPHeaders(t *testing.T) {
+	assert := assert.New(t)
+	header := make(http.Header)
+	header.Add("A", "1")
+	header.Add("A", "2")
+	header.Add("B", "3")
+	header.Add("D", "4")
+	headers := NewHTTPHeaders(header, "D")
+	assert.Equal(3, len(headers))
+	fieldA := ""
+	fieldB := ""
+	for _, item := range headers {
+		k := string(item[0])
+		v := string(item[1])
+		if k == "A" {
+			fieldA += v
+		} else {
+			fieldB += v
+		}
+	}
+	assert.Equal("12", fieldA)
+	assert.Equal("3", fieldB)
+}
+
+func TestHTTPCacheGet(t *testing.T) {
+	t.Run("fetching", func(t *testing.T) {
+		assert := assert.New(t)
+		hc := NewHTTPCache()
+		status, data := hc.Get()
+		assert.Equal(StatusFetching, status)
+		assert.Nil(data)
+	})
+
+	t.Run("hit for pass", func(t *testing.T) {
+		assert := assert.New(t)
+		hc := NewHTTPCache()
+		status, data := hc.Get()
+		assert.Equal(StatusFetching, status)
+		assert.Nil(data)
+		go func() {
+			time.Sleep(time.Millisecond)
+			hc.HitForPass(300)
+		}()
+		// 此时会因为fetching而等待
+		status, data = hc.Get()
+		assert.Equal(StatusHitForPass, status)
+		assert.Equal(StatusHitForPass, hc.status)
+		assert.Nil(data)
+
+		// 此次不会再因为fetching而等待
+		status, data = hc.Get()
+		assert.Equal(StatusHitForPass, status)
+		assert.Equal(StatusHitForPass, hc.status)
+		assert.Nil(data)
+	})
+
+	t.Run("cachable", func(t *testing.T) {
+		assert := assert.New(t)
+		hc := NewHTTPCache()
+		status, data := hc.Get()
+		assert.Equal(StatusFetching, status)
+		assert.Nil(data)
+		statusCode := 200
+		rawBody := []byte("raw body")
+		gzipBody := []byte("gzip body")
+		brBody := []byte("br body")
+		go func() {
+			time.Sleep(time.Millisecond)
+			hc.Cachable(300, statusCode, rawBody, gzipBody, brBody)
+		}()
+		// 此时会因为fetching而等待
+		status, data = hc.Get()
+		assert.Equal(StatusCachable, status)
+		assert.Equal(StatusCachable, hc.status)
+		assert.Equal(statusCode, data.StatusCode)
+		assert.Equal(rawBody, data.RawBody)
+		assert.Equal(gzipBody, data.GzipBody)
+		assert.Equal(brBody, data.BrBody)
+
+		// 此次不会再因为fetching而等待
+		status, data = hc.Get()
+		assert.Equal(StatusCachable, status)
+		assert.Equal(StatusCachable, hc.status)
+		assert.Equal(statusCode, data.StatusCode)
+	})
+}
